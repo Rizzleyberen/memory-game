bc438b7765b0d86f9918590dc11ee72bc898c92c
Silbee <silvanderkooi@gmail.com>
2023-10-11 14:34:53 +0200

    Update project namespace en main window
    De game start nu in fullscreen
    De update functie is voor game logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,31 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace memory_game
-{
+namespace MemoryGame {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
+	public partial class MainWindow : Window {
+		// Hoeveel milliseconden de vorige frame duurde
+		double deltaTime;
+		// Houdt de deltaTime bij
+		Stopwatch stopwatch = new();
+
+		public MainWindow() {
 			InitializeComponent();
+
+			// Voert elke frame de update functie uit
+			CompositionTarget.Rendering += Update;
+
+			// Zet de grid grootte naar de huidige resolutie
+			grid.Width = double.NaN;
+			grid.Height = double.NaN;
+		}
+
+		public void Update(object? sender, EventArgs? e) {
+			// Zorgt ervoor dat deltaTime de lengte is van de vorige frame
+			deltaTime = stopwatch.Elapsed.TotalSeconds;
+			stopwatch.Restart();
 		}
 	}
 }
